a6a2e36a49013b12ffe0b51b817cefe916f8569c
Misael_Rodríguez <misaelrdz963@gmail.com>
2024-11-28 12:50:23 -0600

    subir seeders v2

--- src/seeders/users.py
+++ src/seeders/users.py
@@ -1,3 +1,4 @@
+from decouple import config
 from src.models import User
 from src.service import createUser
 
@@ -5,7 +6,7 @@ def createFirstUser():
     user = User.query.filter_by(usuario="tester00").first()
     if not user:
         try:
-            tester = createUser("el tester", "tester00@gmail.com", "tester00", "tester00")
+            tester = createUser(config("USER"), config("USER_EMAIL"), config("USER_NICKNAME"), config("USER_PASSWORD"))
             print("Usuario creado")
         except Exception as e:
             print(f"Error al crear el usuario: {e}")
